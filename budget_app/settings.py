73dc28d57b423dd3089b3cb076dcfee2056a7021
Matthew Upshaw <matthew.upshaw02@gmail.com>
2022-07-16 16:03:17 +0000

    update settings.py

--- budget_app/settings.py
+++ budget_app/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.adimeadozen.io','adimeadozen.io','34.172.169.19']
 
 
 # Application definition
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'budget_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config['DB_NAME'],
@@ -89,9 +89,9 @@ WSGI_APPLICATION = 'budget_app.wsgi.application'
         'PASSWORD': config['DB_PASSWORD'],
         'HOST': config['CLOUD_SQL_HOST'],
     }
-}'''
+}
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': '127.0.0.1',
@@ -100,7 +100,7 @@ DATABASES = {
         'USER': config['DB_USER'],
         'PASSWORD': config['DB_PASSWORD'],
     }
-}
+}'''
 
 
 # Password validation
